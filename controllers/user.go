1da4009aead09110835369538b5504b5685746a6
Nyae44 <tingadaley@gmail.com>
2024-05-19 10:43:48 +0300

    controllers: add user controller registration

--- controllers/user.go
+++ controllers/user.go
@@ -9,7 +9,7 @@ type userController struct {
 	userIDPattern *regexp.Regexp
 }
 
-func (uc userController) ServeHttp(w http.ResponseWriter, r http.Request) {
+func (uc userController) ServeHttp(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from the user controller!"))
 }
 
